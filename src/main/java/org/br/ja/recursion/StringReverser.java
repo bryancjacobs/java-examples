983fdef20018586fcd48e8178104e1b5b77a65d6
Bryan C Jacobs <bryancjacobs@hotmail.com>
2014-03-15 13:45:24 -0600

    a little recursion...I still struggle thinking this way!!!

---
+++ src/main/java/org/br/ja/recursion/StringReverser.java
@@ -0,0 +1,53 @@
+package org.br.ja.recursion;
+
+/**
+ * User: Bryan
+ * Date: 3/14/14
+ * Time: 3:37 PM
+ */
+public class StringReverser {
+
+    public static void main(String[] args) {
+
+        // uglyReverse
+        String hello = "bryan";
+        System.out.println(uglyReverse(hello, new StringBuilder(), hello.length() - 1));
+
+        // eloquentReverse
+        System.out.println(eloquentReverse(hello));
+
+
+        // loopReverse
+        System.out.println(loopReverse(hello));
+
+
+    }
+
+    public static String uglyReverse(String toReverse, StringBuilder sb, int index){
+        if(index <= -1){
+            return sb.toString();
+        }
+
+        sb.append(toReverse.charAt(index));
+        index--;
+        return uglyReverse(toReverse, sb, index);
+    }
+
+    public static String eloquentReverse(String str) {
+        if ((null == str) || (str.length()  <= 1)) {
+            return str;
+        }
+        return eloquentReverse(str.substring(1)) + str.charAt(0);
+    }
+
+    public static String loopReverse(String toReverse){
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = toReverse.length() - 1; i >= 0; i--) {
+            sb.append(toReverse.charAt(i));
+        }
+
+        return sb.toString();
+
+    }
+}
